0ec0cf56e9c10fe450acb896518ca2e823347c67
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2022-12-17 20:48:25 -0600

    commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# <Your-Project-Title>
+
+## Description
+
+This is my first attempt writing ECMA script.  I am using thie generator to practice and enhance my skills
+
+
+
+## Usage
+
+
+
+`
+    ![alt text](./assets/pwg.png)
+    
+
+## Credits
+programiz.com
+w3schools
+mdn web docs
+
+## License
+
+
+
+## Features
+
+Password Generation
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
+let tlength
 let allchar = [];
 let lowchar =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let highchar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -9,7 +9,8 @@ let spchar = [`!`,`@`,`#`,`$`,`%`,`^`,`&`,`*`];
 let numchar= [0,1,2,3,4,5,6,7,8,9,]
   
 function writePassword() {
-let tlength = window.prompt ("How many characters should be in your Password(Min-8 Max-128)")
+do {tlength = window.prompt ("How many characters should be in your Password(Min-8 Max-128)")} while(tlength < 8 || tlength > 128);
+
   if (window.confirm("would you like lowercase characters")){
     allchar = [...allchar,...lowchar];
   }
