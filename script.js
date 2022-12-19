93372288860b74687cdfd963a5a2af0b3d51970f
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2022-12-19 09:41:01 -0600

    commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 // Write password to the #password input
+let arr
+let password = ""
 let tlength
 let lowerConfirm
 let upperConfirm
@@ -13,46 +15,42 @@ let spchar = [`!`,`@`,`#`,`$`,`%`,`^`,`&`,`*`];
 let numchar= [0,1,2,3,4,5,6,7,8,9,]
 
 function getRandomPw (arr) {
-  arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
-  
 function writePassword() {
 do
 {tlength = window.prompt ("How many characters should be in your Password(Min-8 Max-128, must be a numeric value)")
 console.log(tlength);}
 while (tlength < 8 || tlength > 128 || isNaN(tlength));
-let password = ""
-console.log("step 1")
+
   if (lowerConfirm = window.confirm("would you like lowercase characters")){
     allchar = [...allchar,...lowchar];
-    password += getRandomPw(lowchar)
+    password += getRandomPw(lowchar);
   }
   if (upperConfirm = window.confirm("would you like uppercase characters?")){
     allchar = [...allchar,...highchar];
-    password += getRandomPw(highchar)
+    password += getRandomPw(highchar);
   }
   if (numberConfirm = window.confirm("would you like numbers?")){
     allchar = [...allchar,...numchar];
-    password += getRandomPw(numchar)
+    password += getRandomPw(numchar);
   }
   if (specialConfirm = window.confirm("would you like special characters")){
     allchar = [...allchar,...spchar];
-    password += getRandomPw(spchar)
+    password += getRandomPw(spchar);
   }
   if (!lowerConfirm && !upperConfirm && !numberConfirm && !specialConfirm) {
     window.alert ("Then think of one yourself!")
     return
   }
-
-    for (let i = 0; i< tlength; i++) {
+  let runlength = (tlength - password.length)
+    for  (let i = 0; i< runlength; i++) {
     password += allchar[Math.floor(Math.random() * allchar.length)];
     let passwordText = document.querySelector("#password");
     passwordText.value = password;}
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 /*Tutor Questions
 How to add a MIT license after the fact
-
 */
